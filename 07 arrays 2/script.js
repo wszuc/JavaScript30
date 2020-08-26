ca332945c69f74af9d1ad8cacfc215737dd3ebe4
wszuc <wiktor.szuca@gmail.com>
2020-08-26 23:09:37 +0200

    New cool challenge completed

---
+++ 07 arrays 2/script.js
@@ -0,0 +1,48 @@
+const people = [
+    { name: 'Wes', year: 1988 },
+    { name: 'Kait', year: 1986 },
+    { name: 'Irv', year: 1970 },
+    { name: 'Lux', year: 2015 }
+  ];
+
+  const comments = [
+    { text: 'Love this!', id: 523423 },
+    { text: 'Super good', id: 823423 },
+    { text: 'You are the best', id: 2039842 },
+    { text: 'Ramen is my fav food ever', id: 123523 },
+    { text: 'Nice Nice Nice!', id: 542328 }
+  ];
+
+  console.log('So you are curious! That\'s good! 🌮');
+   // Some and Every Checks
+    // Array.prototype.some() // is at least one person 19 or older?
+    /*function ifAdult(person){
+        const currentYear = new Date().getFullYear();
+        return currentYear - person.year;
+    }
+    
+    const isAdult = people.some(function(person){
+        if(ifAdult(person) >= 19){
+            return true;
+        }
+
+    });*/
+
+    const isAdult = people.some(person => new Date().getFullYear() - person.year >= 19);
+    console.log(isAdult);
+    // Array.prototype.every() // is everyone 19 or older?
+    const areAdults = people.every(person => new Date().getFullYear() - person.year >= 19);
+    console.log(areAdults)
+    // Array.prototype.find()
+    // Find is like filter, but instead returns just the one you are looking for
+    // find the comment with the ID of 823423
+    const desiredComment = comments.find(comment => comment.id === 823423);
+    console.log(desiredComment);
+
+    // Array.prototype.findIndex()
+    // Find the comment with this ID
+    // delete the comment with the ID of 823423
+    const index = comments.findIndex(comment => comment.id === 823423);
+    console.log(index);
+    comments.splice(index, 1);
+    console.table(comments);
